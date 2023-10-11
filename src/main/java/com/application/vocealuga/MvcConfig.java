93dfe169cbb797c4de5a75dd1bd141ce5b72a404
dev-guilherme <guilhermepereiradv@gmail.com>
2023-10-10 22:48:04 -0300

    feat: add mvc config

---
+++ src/main/java/com/application/vocealuga/MvcConfig.java
@@ -0,0 +1,12 @@
+package com.application.vocealuga;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+    public void addViewControllers(org.springframework.web.servlet.config.annotation.ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/cadastro").setViewName("cadastro");
+    }
+}
