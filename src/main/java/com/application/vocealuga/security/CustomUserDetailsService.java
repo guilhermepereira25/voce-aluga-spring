035551d641cafe81422d28932d78e9d21b4b4daa
dev-guilherme <guilhermepereiradv@gmail.com>
2023-10-24 20:16:17 -0300

    feat: customUserDetailsServices

---
+++ src/main/java/com/application/vocealuga/security/CustomUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.application.vocealuga.security;
+
+
+import com.application.vocealuga.entity.ClienteEntity;
+import com.application.vocealuga.repository.ClienteRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private ClienteRepository clienteRepository;
+
+    public CustomUserDetailsService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String document) throws UsernameNotFoundException {
+        ClienteEntity clienteEntity = clienteRepository.findByCpf(document);
+
+        if (clienteEntity == null) {
+            clienteEntity = clienteRepository.findByCnpj(document);
+
+            if (clienteEntity == null) {
+                throw new UsernameNotFoundException("Cliente não encontrado");
+            }
+        }
+
+        return new User(
+                clienteEntity.getNome(),
+                clienteEntity.getSenha(),
+                clienteEntity.getPermissao().stream().map((role) -> new SimpleGrantedAuthority(role.getNome()))
+                        .collect(Collectors.toList())
+        );
+    }
+}
