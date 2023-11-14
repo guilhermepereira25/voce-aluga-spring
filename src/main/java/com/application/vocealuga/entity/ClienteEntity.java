950c32e29da45e55b0417f16e7e4ee6984fba692
dev-guilherme <guilhermepereiradv@gmail.com>
2023-11-13 22:42:00 -0300

    refactor: import lombok builder

--- src/main/java/com/application/vocealuga/entity/ClienteEntity.java
+++ src/main/java/com/application/vocealuga/entity/ClienteEntity.java
@@ -1,15 +1,13 @@
 package com.application.vocealuga.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Collection;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
--- src/main/java/com/application/vocealuga/entity/Permissao.java
+++ src/main/java/com/application/vocealuga/entity/Permissao.java
@@ -1,16 +1,14 @@
 package com.application.vocealuga.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
