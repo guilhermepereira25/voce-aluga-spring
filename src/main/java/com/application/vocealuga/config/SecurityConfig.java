fb44995370d3937fbcefece6f98380f061dbb892
dev-guilherme <guilhermepereiradv@gmail.com>
2023-11-21 22:04:40 -0300

    chore: fix authority

--- src/main/java/com/application/vocealuga/config/SecurityConfig.java
+++ src/main/java/com/application/vocealuga/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/cadastro")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/cadastrar")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/cadastroVeiculo")).hasRole("ADMIN")
+                        .requestMatchers(new AntPathRequestMatcher("/cadastroVeiculo")).hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
