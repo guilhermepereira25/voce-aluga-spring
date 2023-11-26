4411f1a7f62f2100b2fa19af89b2867ad7ff6fba
dev-guilherme <guilhermepereiradv@gmail.com>
2023-11-25 21:12:10 -0300

    feat: add devolucao logic in VeiculoController

--- src/main/java/com/application/vocealuga/controller/VeiculoController.java
+++ src/main/java/com/application/vocealuga/controller/VeiculoController.java
@@ -1,5 +1,6 @@
 package com.application.vocealuga.controller;
 
+import com.application.vocealuga.dto.VeiculoSearchDto;
 import com.application.vocealuga.dto.VeiculoDto;
 import com.application.vocealuga.entity.Veiculo;
 import com.application.vocealuga.service.impl.AgenciaServiceImpl;
@@ -61,5 +62,60 @@ public class VeiculoController {
 
         model.addAttribute("veiculo", veiculo);
         return "reserva";
+    }
+
+    @GetMapping("/veiculo/verificar")
+    public String verificarVeiculo(@ModelAttribute VeiculoSearchDto searchVeiculoDto, @RequestParam(name = "placa", required = false) String placa, Model model) {
+        if (placa != null && !placa.isEmpty()) {
+            Veiculo veiculo = veiculoService.findByPlaca(placa);
+            if (veiculo == null) {
+                return "redirect:/veiculo/verificar?error=1";
+            }
+            model.addAttribute("veiculo", veiculo);
+        } else if (searchVeiculoDto.getPlaca() != null && !searchVeiculoDto.getPlaca().isEmpty()) {
+            Veiculo veiculo = veiculoService.findByPlaca(searchVeiculoDto.getPlaca());
+            if (veiculo == null) {
+                return "redirect:/veiculo/verificar?error=1";
+            }
+            model.addAttribute("veiculo", veiculo);
+        } else if (searchVeiculoDto.getId() != null && searchVeiculoDto.getId() > 0) {
+            Veiculo veiculo = veiculoService.findById(searchVeiculoDto.getId());
+            if (veiculo == null) {
+                return "redirect:/veiculo/verificar?error=1";
+            }
+            model.addAttribute("veiculo", veiculo);
+        } else if (searchVeiculoDto.getStatus() != null && !searchVeiculoDto.getStatus().isEmpty()) {
+            model.addAttribute("veiculos", veiculoService.findBySituacao(searchVeiculoDto.getStatus()));
+            model.addAttribute("veiculoDto", new VeiculoSearchDto());
+            return "verificar-veiculo";
+        }
+
+        model.addAttribute("veiculos", veiculoService.findAll());
+        model.addAttribute("veiculoDto", new VeiculoSearchDto());
+        return "verificar-veiculo";
+    }
+
+    @RequestMapping("/veiculo/atualizar/{id}")
+    public String atualizarVeiculo(@PathVariable("id") Long id, Model model) {
+        Veiculo veiculo = veiculoService.findById(id);
+
+        if (veiculo == null) {
+            return "redirect:/catalogoVeiculos";
+        }
+
+        model.addAttribute("veiculo", veiculo);
+        return "atualizar-veiculo";
+    }
+
+    @GetMapping("/veiculo/devolver/{id}")
+    public String devolverVeiculo(@PathVariable("id") Long id, Model model) {
+        Veiculo veiculo = veiculoService.findById(id);
+        if (veiculo == null) {
+            return "redirect:/atualizar-veiculo?error=1";
+        }
+
+        model.addAttribute("devolucaoDto", new DevolucaoDto());
+        model.addAttribute("veiculo", veiculo);
+        return "devolucao";
     }
 }
